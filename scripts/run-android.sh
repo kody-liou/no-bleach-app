d4045e6c8ddf57e65be86958e69817ea94ded56a
Kody Liou <kody.liou@gmail.com>
2024-07-07 01:04:01 +0800

    Update main script

--- scripts/run-android.sh
+++ scripts/run-android.sh
@@ -3,8 +3,14 @@
 echo "===================================="
 source "$(dirname "$0")/helpers/install-jdk.sh"
 echo "===================================="
+source "$(dirname "$0")/helpers/android-home.sh"
+echo "===================================="
 source "$(dirname "$0")/helpers/install-dependencies.sh"
 echo "===================================="
 npx vite build
+echo "===================================="
+echo "sync android"
 npx cap sync android
-npx cap run android
+echo "===================================="
+echo "run android"
+npx cap run android --info
